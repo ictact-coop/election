34a856a3466be1a3d0fecc1eec578987b1843004
h2dj <h2dj@ictact.kr>
2021-03-10 01:12:04 +0900

    후보 기본 페이지를 등록 페이지로 / 등록 후 등록결과(신청서) 표시

--- election/candidates/views.py
+++ election/candidates/views.py
@@ -8,16 +8,18 @@ from django.conf import settings
 import base64
 
 def index(request):
-    return render(request, 'candidates/index.html')
+    # return render(request, 'candidates/index.html')
+    return redirect('candidates:register')
 
 def register(request):
     message = ''
+    registration_result = None
 
     if request.method == 'POST':
         form = RegistrationForm(request.POST, request.FILES)
         if form.is_valid():
             registration = form.save(commit=False)
-            # 추천 제한 수를 넘었는지 확인
+            # 이미 등록되어 있는지 확인 (현재 동명이인 없으므로 이름만 확인)
             if Candidate.objects.filter(candidate_name=registration.candidate_name).exists():
                 message = registration.candidate_name + '님은 이미 후보로 등록되어 있습니다. 등록 내용을 변경하려면 선관위에 문의해주세요'
             else:
@@ -27,16 +29,18 @@ def register(request):
                 if not registration.signature:
                     if image_data != '':
                         filepath = getattr(settings, 'MEDIA_ROOT', None)
-                        filename = filepath + '/' + 'new_sign_' + registration.candidate_name + '.png'
+                        filename = 'new_sign_' + registration.candidate_name + '.png'
+                        real_filename = filepath + '/' + filename
                         # 헤더 부분 제거
                         image_data = image_data[22:]
-                        new_signature = open(filename, 'wb')
+                        new_signature = open(real_filename, 'wb')
                         new_signature.write(base64.b64decode(image_data))
                         new_signature.close()
-                        registration.signature = filename
+                        registration.signature = getattr(settings, 'MEDIA_URL', None) + filename
                 registration.save()
                 message = registration.candidate_name + '님의 임원 후보 등록 신청이 접수되었습니다. 조합원 3명 이상의 추천을 모아주시면 선관위에서 확인 후 공식 후보로 등록됩니다.'
             form = RegistrationForm()
+            registration_result = registration
 
         else:
             message = '후보 등록 신청에 실패했습니다. 다시 확인해주세요'
@@ -44,7 +48,7 @@ def register(request):
     else:
         form = RegistrationForm()
 
-    context = { 'form': form, 'message': message }
+    context = { 'form': form, 'message': message, 'registration_result': registration_result }
 
     return render(request, 'candidates/register.html', context)
 
